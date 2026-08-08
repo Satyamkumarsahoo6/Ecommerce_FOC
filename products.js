6e69d24479242fae9ecce9b2594a086fec16e82c
SATYAM KUMAR SAHOO <Sahoosatyamkumar963@gmail.com>
2026-08-08 15:57:35 +0530

    Add products data

---
+++ products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: 'Wireless Headphones',
+    price: 2499,
+    image: 'https://picsum.photos/seed/headphones/400/300',
+  },
+  {
+    id: 2,
+    name: 'Smart Watch',
+    price: 3999,
+    image: 'https://picsum.photos/seed/watch/400/300',
+  },
+  {
+    id: 3,
+    name: 'Classic T-Shirt',
+    price: 799,
+    image: 'https://picsum.photos/seed/tshirt/400/300',
+  },
+];
+
+export default products;
