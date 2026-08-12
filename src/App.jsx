01db4b14103d53b076ee474eb2d35d4f32bfb2da
SATYAM KUMAR SAHOO <Sahoosatyamkumar963@gmail.com>
2026-08-12 09:16:22 +0530

    deployment fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 import Electronics from "./pages/Electronics";
 import Fashion from "./pages/Fashion";
 import ProductDetails from "./pages/ProductDetails";
